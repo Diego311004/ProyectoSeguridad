90b0ee6eaf060ba5822d89c53c6220359519df22
Diego311004 <diego311004ramirez@gmail.com>
2025-04-29 22:04:44 -0600

    Primer commit: agregar archivos de login y registro

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const users = JSON.parse(localStorage.getItem('users')) || [];
+
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Guardar el usuario
+    users.push({ username, password });
+    localStorage.setItem('users', JSON.stringify(users));
+
+    console.log(`Usuario registrado: ${username}`);
+    gtag('event', 'registro', { 'usuario': username });
+    alert('Registro exitoso');
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const loginUsername = document.getElementById('loginUsername').value;
+    const loginPassword = document.getElementById('loginPassword').value;
+
+    const user = users.find(u => u.username === loginUsername && u.password === loginPassword);
+
+    if (user) {
+        console.log(`Usuario iniciado sesión: ${loginUsername}`);
+        gtag('event', 'inicio_sesion', { 'usuario': loginUsername });
+        window.location.href = 'home.html';
+    } else {
+        alert('Nombre de usuario o contraseña incorrectos');
+    }
+});
